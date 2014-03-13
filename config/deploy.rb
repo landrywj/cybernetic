3f41914c12df3bff84f5ee8cd6b8778f4099ba7a
Wilfrid Landry <landrywj@me.com>
2014-03-13 14:41:01 -0700

    back to capistrano 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,70 +1,52 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+require "rvm/capistrano"
+require "bundler/capistrano"
 
-set :application, 'cybernetic'
-set :deploy_user, 'deployer'
+server "107.170.248.49", :web, :app, :db, primary: true
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, "cybernetic"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/home/deployer/apps/#{application}"
+set :scm, "git"
+set :repository, "git@github.com:landrywj/#{application}.git"
+set :branch, "master"
 
-# Default value for :scm is :git
-# set :scm, :git
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-#set :default_env, { path: "/home/deployer/.rvm/rubies/ruby-2.1.0:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
 
-  task :setup_config do
-    on roles(:app) do
+  task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
-    end
   end
-  
+  after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config do
-    on  roles(:app) do
+  task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    end
   end
-
+  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:web) do
+  task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
     end
-    end
   end
   before "deploy", "deploy:check_revision"
-
-end  
+end
