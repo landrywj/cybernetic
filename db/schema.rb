1bc62cd50ac3cbcf5b2564880bc81f9d8fa9d3c0
Wilfrid Landry <landrywj@me.com>
2014-03-17 13:36:53 -0700

    destroy contacts model and start over

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317160715) do
+ActiveRecord::Schema.define(version: 20140317172545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "display_name"
+    t.string   "nickame"
+    t.string   "primary_email"
+    t.string   "secondary_email"
+    t.string   "screen_name"
+    t.string   "work_phone"
+    t.string   "home_phone"
+    t.string   "fax_number"
+    t.string   "mobile_number"
+    t.string   "home_address"
+    t.string   "home_address2"
+    t.string   "home_city"
+    t.string   "home_state"
+    t.string   "home_zip_code"
+    t.string   "home_country"
+    t.string   "work_address"
+    t.string   "work_address_2"
+    t.string   "work_city"
+    t.string   "work_state"
+    t.string   "work_zip_code"
+    t.string   "work_country"
+    t.string   "job_title"
+    t.string   "department"
+    t.string   "organization"
+    t.string   "web_page_1"
+    t.string   "web_page_2"
+    t.string   "birth_year"
+    t.string   "birth_month"
+    t.string   "birth_day"
+    t.string   "custom_1"
+    t.string   "custom_2"
+    t.string   "custom_3"
+    t.string   "custom_4"
+    t.text     "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contacts", ["last_name"], name: "index_contacts_on_last_name", using: :btree
 
   create_table "recipes", force: true do |t|
     t.string   "technology"
