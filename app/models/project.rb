40b8203ff1f149deea0ba0cf87bbc7cd43771ca2
Wilfrid Landry <landrywj@me.com>
2014-03-28 16:56:19 -0700

    In the middle of nesting resources. Finish it next time

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
 	has_many :devlogs
+	has_many :tasks 
+	has_many :use_cases
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,29 @@
 Cybernetic::Application.routes.draw do
-  resources :tasks
+  
+  root 'projects#index'
+  
+  devise_for :users
 
-  resources :use_cases
-
-  resources :devlogs
-
-  resources :projects
+  resources :projects do 
+    resources :tasks
+    resources :use_cases
+    resources :devlogs
+  end
+  
+  resources :snippets
 
   resources :contacts
-
   resources :recipes
 
-  #devise_for :users
    #temporarily disable registration until site is ready. 
-  devise_for :users #:controllers => {:registrations => "registrations"}
-  resources :snippets
+   #:controllers => {:registrations => "registrations"}
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'snippets#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
