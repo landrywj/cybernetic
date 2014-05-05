8eeebc868a2b02b2fb2d0e6cc9186b4c93895a24
Wilfrid Landry <landrywj@me.com>
2014-05-05 16:51:21 -0700

    new configs for unicorn multi-app server env and new index for nginx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 root = "/home/atlaswarner/apps/cybernetic/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn-cybernetic.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
